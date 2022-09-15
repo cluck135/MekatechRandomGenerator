f5a58e940e9a2f70fa36b97818b3ea57fe0ec311
twenty-five <twenty-five@SAMSUNG-SM-G930V.localdomain>
2022-09-15 13:36:16 -0400

    added random generative NFTs

--- main.js
+++ main.js
@@ -1,9 +1,10 @@
 import './style.css';
 import * as THREE from 'three';
-import { ethers } from "ethers";
+import  { OBJLoader }  from 'three/examples/jsm/loaders/OBJLoader';
+import { MTLLoader } from 'three/examples/jsm/loaders/MTLLoader.js';
+import { ColladaExporter } from 'three/examples/jsm/exporters/ColladaExporter';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 import myEpikNft from './utils/MyEpikNFT.json'
-import { GridHelper } from 'three';
 const CONTRACT_ADDRESS = "0x71964621a255F1da7ebde644F36258Cf365174dF";
 
 const scene = new THREE.Scene();
@@ -17,24 +18,9 @@ const renderer = new THREE.WebGLRenderer({
 
 renderer.setPixelRatio(window.devicePixelRatio);
 renderer.setSize(window.innerWidth, window.innerHeight);
-camera.position.setZ(30);
-// fuckn around 
-
-var a = new THREE.Vector3( 1, 0, 0 );
-var b = new THREE.Vector3( 0, 1, 0 );
-
-var c = new THREE.Vector3();
-c.crossVectors( a, b );
-
-scene.add(c)
-
-// Torus
-
-const geometry = new THREE.TorusGeometry(3, 0.4, 16, 100);
-const material = new THREE.MeshStandardMaterial({ color: 0xff6347 });
-const torus = new THREE.Mesh(new THREE.TorusGeometry(3, 0.4, 16, 100), new THREE.MeshStandardMaterial({ color: 0xff6347 }));
-const torus2 = new THREE.Mesh(new THREE.TorusGeometry(3, 0.4, 16, 100), new THREE.MeshStandardMaterial({ color: 0xff00ff }));
-const torus3 = new THREE.Mesh(new THREE.TorusGeometry(3, 0.4, 16, 100), new THREE.MeshStandardMaterial({ color: 0x006699 }));
+camera.position.setZ(-1);
+camera.position.setY(0)
+camera.position.setX(4)
 
 
 // Lights
@@ -58,6 +44,131 @@ renderer.render(scene, camera);
 
 const jeffTexture = new THREE.TextureLoader().load('jeff.png');
 
+// adding Head Sphere 
+const head = new THREE.Mesh(
+  new THREE.SphereGeometry(2, 32, 32),
+  new THREE.MeshStandardMaterial({ color: 0x8dd3dd }))
+scene.add(head)
+//Loading OBJ and accompanying MTL
+
+let eyeArray = [
+  {name: 'eyes1', type: 'eye'}, 
+  {name: 'eyes2', type: 'eye'}, 
+  {name: 'eyes3', type: 'eye'}, 
+]
+
+let noseArray = [
+  {name: 'nose1', type: 'nose'}, 
+  {name: 'nose2', type: 'nose'}, 
+  {name: 'nose3', type: 'nose'}, 
+]
+
+let mouthArray = [
+  {name: 'mouth1', type: 'mouth'}, 
+  {name: 'mouth2', type: 'mouth'}, 
+  {name: 'mouth3', type: 'mouth'}, 
+]
+
+const objLoader = new OBJLoader();
+const mtlLoader = new MTLLoader();
+const collExporter = new ColladaExporter();
+let i = 0
+// IN the Future loop one of these functions so that it goes through an array of each MTL file and OBJ and adds to the scene
+async function loadRndFace( asset ) {
+    let mtl = await mtlLoader.loadAsync(`./faceMtls/${asset.name}.mtl`);
+    mtl.preload();
+    objLoader.setMaterials(mtl);
+    let obj = await objLoader.loadAsync(`./faceObjs/${asset.name}.obj`);
+    scene.add(obj);
+    switch (asset.type) {
+      case 'eye':
+        obj.position.y = 0.5
+        obj.position.x = 1.2
+        break;
+      case 'nose':
+        obj.position.x = 1.23
+        break;
+      default:
+        obj.position.y = -0.5
+        obj.position.x = 1.18
+        break;
+    }
+}  
+
+// async function downloadFile(data) {
+
+// }
+
+(async () => {
+    let rndInt = Math.floor(Math.random() * 3) 
+    await loadRndFace(eyeArray[rndInt])
+     rndInt = Math.floor(Math.random() * 3) 
+    await loadRndFace(noseArray[rndInt])
+     rndInt = Math.floor(Math.random() * 3) 
+    await loadRndFace(mouthArray[rndInt])
+})()
+
+const link = document.createElement( 'a' );
+			link.style.display = 'none';
+			document.body.appendChild( link );
+
+const exporter = new ColladaExporter();
+
+function exportCollada() {
+
+  const result = exporter.parse( scene, undefined, { upAxis: 'Y_UP', unitName: 'millimeter', unitMeter: 0.001 } );
+  let materialType = 'Phong';
+
+  // if ( shading === 'wireframe' ) {
+
+  //   materialType = 'Constant';
+
+  // }
+
+  // if ( shading === 'smooth' ) {
+
+  //   materialType = 'Lambert';
+
+  // }
+
+  saveString( result.data, 'face' + '.dae' );
+
+  result.textures.forEach( tex => {
+
+    saveArrayBuffer( tex.data, `${ tex.name }.${ tex.ext }` );
+
+  } );
+
+}
+
+function save( blob, filename ) {
+
+  link.href = URL.createObjectURL( blob );
+  link.download = filename;
+  link.click();
+
+}
+
+function saveString( text, filename ) {
+
+  save( new Blob( [ text ], { type: 'text/plain' } ), filename );
+
+}
+
+function saveArrayBuffer( buffer, filename ) {
+
+  save( new Blob( [ buffer ], { type: 'application/octet-stream' } ), filename );
+
+}
+
+
+// Torus
+
+const geometry = new THREE.TorusGeometry(3, 0.4, 16, 100);
+const material = new THREE.MeshStandardMaterial({ color: 0xff6347 });
+const torus = new THREE.Mesh(new THREE.TorusGeometry(3, 0.4, 16, 100), new THREE.MeshStandardMaterial({ color: 0xff6347 }));
+const torus2 = new THREE.Mesh(new THREE.TorusGeometry(3, 0.4, 16, 100), new THREE.MeshStandardMaterial({ color: 0xff00ff }));
+const torus3 = new THREE.Mesh(new THREE.TorusGeometry(3, 0.4, 16, 100), new THREE.MeshStandardMaterial({ color: 0x006699 }));
 
 // CUBE making
 const cube = new THREE.Mesh(new THREE.BoxGeometry(3, 3, 3), new THREE.MeshStandardMaterial({ color: 0xff4447 }));
@@ -93,91 +204,129 @@ const moon3 = new THREE.Mesh(
   })
 );
 
-//ADDING to scene
+// //Torus add to scene
 
-function addTorus() {
-  const rndInt = Math.floor(Math.random() * 3) + 1
+// function addTorus() {
+//   const rndInt = Math.floor(Math.random() * 3) + 1
+//   switch (rndInt) {
+//     case 1:
+//       scene.add(torus)
+//       torus.rotation.set(2,0,0)
+//       torus.position.z = -5
 
-  // add javascript switch case
-}
+//       break;
+//     case 2: 
+//       scene.add(torus2)
+//       torus2.rotation.set(2,0,0)
+//       torus2.position.z = -10
 
-scene.add(torus);
-scene.add(torus2);
-scene.add(torus3);
+//       break;
+//     default:
+//       scene.add(torus3)
+//       torus3.rotation.set(2,0,0)
+//       torus3.position.z = -15
+
+//       break;
+//   }
+// }
+
+// //Cube add to scene
+// function addCube() {
+//   const rndInt = Math.floor(Math.random() * 3) + 1
+//   switch (rndInt) {
+//     case 1:
+//       scene.add(cube)
+//       cube.position.x = -5
+
+//       break;
+//     case 2: 
+//       scene.add(cube2)
+//       cube2.position.x = -10
+
+//       break;
+//     default:
+//       scene.add(cube3)
+//       cube3.position.x = -15
+
+//       break;
+//   }
+// }
+
+// //Moon add to scene
+// function addMoon() {
+//   const rndInt = Math.floor(Math.random() * 3) + 1
+//   switch (rndInt) {
+//     case 1:
+//       scene.add(moon)
+//       moon.position.x = 5
+
+//       break;
+//     case 2: 
+//       scene.add(moon2)
+//       moon2.position.x = 10
+
+//       break;
+//     default:
+//       scene.add(moon3)
+//       moon3.position.x = 15
+
+//       break;
+//   }
+// }
+
+// addTorus()
+// addCube()
+// addMoon()
 
 
 
-scene.add(cube);
-scene.add(cube2);
-scene.add(cube3);
-
-scene.add(moon);
-scene.add(moon2);
-scene.add(moon3)
-
-torus.rotation.set(2,0,0)
-torus2.rotation.set(2,0,0)
-torus3.rotation.set(2,0,0)
-
-torus.position.z = -5
-torus2.position.z = -10
-torus3.position.z = -15
-
-cube.position.x = -5
-cube2.position.x = -10
-cube3.position.x = -15
-
-moon.position.x = 5
-moon2.position.x = 10
-moon3.position.x = 15
 
 
 
 
+// let strDownloadMime = "image/octet-stream";
 
-let strDownloadMime = "image/octet-stream";
+// // save file button
 
-// save file button
+// let saveFile = async (strData, filename)  => {
+//   let link = document.createElement('a');
+//   if (typeof link.download === 'string') {
+//       document.body.appendChild(link); //Firefox requires the link to be in the body
+//       link.download = filename;
+//       link.href = strData;
+//       link.click();
+//       //await askContractToMintNft(strData)
 
-let saveFile = async (strData, filename)  => {
-  let link = document.createElement('a');
-  if (typeof link.download === 'string') {
-      document.body.appendChild(link); //Firefox requires the link to be in the body
-      link.download = filename;
-      link.href = strData;
-      link.click();
-      //await askContractToMintNft(strData)
+//       document.body.removeChild(link); //remove the link when done
 
-      document.body.removeChild(link); //remove the link when done
+//   } else {
+//       location.replace(uri);
+//   }
+// }
 
-  } else {
-      location.replace(uri);
-  }
-}
+// function saveAsImage() {
+//   let imgData, imgNode;
 
-function saveAsImage() {
-  let imgData, imgNode;
-
-  try {
-      var strMime = "image/jpeg";
-      imgData = renderer.domElement.toDataURL(strMime);
-      // contextt = renderer.domElement.getContext("2d");
-      // let something = contextt.getSVG();
-    //   function drawCanvas(ctx) {
-    //     ctx.fillStyle = "#FF0000";
-    //     ctx.fillRect(0,0,150,75);
-    // }
-    // drawCanvas(canvasContext); 
+//   try {
+//       var strMime = "image/jpeg";
+//       imgData = renderer.domElement.toDataURL(strMime);
+//       // contextt = renderer.domElement.getContext("2d");
+//       // let something = contextt.getSVG();
+//     //   function drawCanvas(ctx) {
+//     //     ctx.fillStyle = "#FF0000";
+//     //     ctx.fillRect(0,0,150,75);
+//     // }
+//     // drawCanvas(canvasContext); 
       
-      saveFile(imgData.replace(strMime, strDownloadMime), "test.jpg");
+//       saveFile(imgData.replace(strMime, strDownloadMime), "test.jpg");
 
-  } catch (e) {
-      console.log(e);
-      return;
-  }
-}
+//   } catch (e) {
+//       console.log(e);
+//       return;
+//   }
+// }
 
-document.getElementById("saveLink").addEventListener('click', saveAsImage);
+document.getElementById("saveLink").addEventListener('click', exportCollada);
 
 
 const connectWallet = async () => {
